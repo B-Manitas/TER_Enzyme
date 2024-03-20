7137d2a2ada8a146500d0d9d2ab52af73753d315
Manitas Bahri <manitas.bhr@gmail.com>
2024-03-20 12:04:27 +0100

    refactor: clean the main

--- main.cpp
+++ main.cpp
@@ -1,41 +1,14 @@
-#include "include/lexer.hpp"
-#include "include/parser.hpp"
+#include "include/simulation.hpp"
+#include "include/view.hpp"
 
-int main(const int argc, const char **argv)
+int main(int argc, char **argv)
 {
-    // Open the file
-    FILE *fp;
+    Simulation simulation = Simulation();
+    simulation.init(argv[1]);
 
-    // Check if the file name is missing
-    if (argc < 2)
-    {
-        printf("Error: the file name is missing\n");
-        return 1;
-    }
-
-    else
-        fp = fopen(argv[1], "r");
-
-    // Check if the file could not be opened
-    if (fp == NULL)
-    {
-        printf("Error: the file could not be opened\n");
-        return 1;
-    }
-
-    Lexer lexer = Lexer();
-    Parser parser = Parser();
-    std::vector<UL> data_tokenized = lexer.lex_all(fp);
-
-    std::vector<instr> instructions = std::vector<instr>{};
-    std::vector<react> reactions = std::vector<react>{};
-
-    parser.parse(data_tokenized, reactions, instructions);
-
-    parser.print_react(reactions);
-    parser.print_instr(instructions);
-
-    fclose(fp);
+    View view = View(simulation);
+    view.init_opengl(argc, argv);
+    view.display();
 
     return 0;
 }
